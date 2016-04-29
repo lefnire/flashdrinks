0dd1d6d44bcac46bf40cf699b9aec6d0ee4a0238
Tyler Renelle <tylerrenelle@gmail.com>
2016-04-29 16:49:54 -0400

    fixes

--- src/js/config.js
+++ src/js/config.js
@@ -8,7 +8,8 @@ angular.module('app.config', [])
   .constant('FBSITE', 'https://flashdrinksapp.com')
 
   // See https://github.com/lefnire/server-misc
-  .constant('SERVER', 'https://flashdrinks-server.herokuapp.com')
+  //.constant('SERVER', 'https://flashdrinks-server.herokuapp.com')
+  .constant('SERVER', 'http://127.0.0.1:3000')
 
   //See https://assembly.com/flashdrinks/metrics/snippet
   .constant('ASM_METRICS', '48247aa736075991c0a88e67e7fc9257175ebc77')
